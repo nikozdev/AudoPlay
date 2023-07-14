9e83bf0ee3b93c014aabd7e2fcb8ff3c4ecbfa1c
nikozdev <nikozdev@nikozdev.net>
2023-07-14 16:56:27 -0700

    !lib!deps!audio!dsp!bass
    [aSoundAgent.csProj] > added ManagedBass nuget package reference
    [src/main.cs] > writing a basic test of the bass library

--- src/main.cs
+++ src/main.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// headers
+using ManagedBass;
+// datadef
+var vRootPath = System.IO.Directory.GetCurrentDirectory();
+Console.WriteLine(
+    String.Format(
+        "what file should i play ? (relative to {0}/)",
+        vRootPath
+    )
+);
+var vFilePath = String.Format("{0}/{1}", vRootPath, Console.ReadLine());
+// actions
+#if true
+Bass.Init(); // Initialize with default options.
+var vFileHandle = Bass.CreateStream(vFilePath);
+Bass.ChannelPlay(vFileHandle);
+Console.ReadKey(); // Wait till user presses a Key.
+Bass.ChannelStop(vFileHandle);
+Bass.Free(); // Free the device.
+#endif
