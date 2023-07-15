8ca39f2d8e1a7d822291d17dd7592fcc71bca5d4
nikozdev <nikozdev@nikozdev.net>
2023-07-15 01:44:35 -0700

    !src!main!test!echo!audio!sound!callback!portaudio!build!config!cxx
    [src/main.cxx] > implemented Echo and IModify callbacks
    > refactoring
    > added some user input
    > added device info output
    > added i-o device index setting which for now just prints the info
    [CMakeLists.txt] > fixed cxx standard setting and added -Wno-format
    > changed the minor version

--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,7 @@
 // headers
 // // standard
 #include <cstdio>
+#include <iostream>
 #include <limits>
 // // 3rd-party
 #include "../lib/portaudio/include/portaudio.h"
@@ -21,14 +22,15 @@ using tECode		= ::PaError;
 // consdef
 constexpr auto cNull = nullptr;
 // // errors
-constexpr auto cECodeNone = ::paNoError;
+constexpr auto cECodeNone	   = ::paNoError;
+constexpr auto cECodeADevIndex = 0x10;
 // // buffers
-constexpr auto cBufferFrameCount = 0xff; // paFramesPerBufferUnspecified
+constexpr auto cBufferFrameCount = paFramesPerBufferUnspecified;
 // // channels
-constexpr unsigned cIChanCount = 0;
+constexpr unsigned cIChanCount = 1;
 constexpr unsigned cOChanCount = 2;
 // // samples
-constexpr unsigned		   cSampleRate = 44'100;
+constexpr unsigned		   cSampleRate = 8'000;
 constexpr ::PaSampleFormat cSampleEnum = paInt8;
 // // // limits
 constexpr tSample cSampleMin = tSampleLimits::min();
@@ -44,12 +46,7 @@ typedef struct tTestData
 	tSample vPhaseR;
 } tTestData;
 // actions
-/* this routine will be called by the audio engine when audio is needed
- * it may called at interrupt level on some machines
- * > so don't do anything that could mess up the system
- * > > like calling malloc() or free()
- */
-static int fTestCallback(
+static int fWaveSawCallback(
 	const void						 *pBufferI,
 	void							 *pBufferO,
 	unsigned long					  vBufferFrameCount,
@@ -58,16 +55,14 @@ static int fTestCallback(
 	void							 *pUserData
 )
 {
-	// unused
-	(void)pBufferI;
-	// get the stream-passed data
-	tTestData *pData = (tTestData *)pUserData;
-	tSample	  *pIter = (tSample *)pBufferO;
+	tTestData *pData  = (tTestData *)pUserData;
+	tSample	  *pIterO = (tSample *)pBufferO;
+	tSample	  *pIterI = (tSample *)pBufferI;
 	// write to the buffer
 	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
 	{
-		*(pIter++) = pData->vPhaseL;
-		*(pIter++) = pData->vPhaseR;
+		*(pIterO++) = pData->vPhaseL;
+		*(pIterO++) = pData->vPhaseR;
 		// simple sawtooth phaser that ranges between min and max
 		pData->vPhaseL += cSampleNegStep;
 		if(pData->vPhaseL <= cSampleMin) { pData->vPhaseL -= cSampleMin; }
@@ -75,7 +70,84 @@ static int fTestCallback(
 		if(pData->vPhaseR >= cSampleMax) { pData->vPhaseR -= cSampleMax; }
 	} // write to the buffer
 	return cECodeNone;
-}
+} // fWaveSawCallback
+static int fEchoCallback(
+	const void						 *pBufferI,
+	void							 *pBufferO,
+	unsigned long					  vBufferFrameCount,
+	const ::PaStreamCallbackTimeInfo *pTimeInfo,
+	::PaStreamCallbackFlags			  vStatusFlags,
+	void							 *pUserData
+)
+{
+	tSample *pIterO = (tSample *)pBufferO;
+	tSample *pIterI = (tSample *)pBufferI;
+	// write to the buffer
+	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
+	{
+		*(pIterO++) = *(pIterI);
+		*(pIterO++) = *(pIterI);
+		pIterI++;
+	} // write to the buffer
+	return cECodeNone;
+} // fEchoCallback
+static int fIModifyCallback(
+	const void						 *pBufferI,
+	void							 *pBufferO,
+	unsigned long					  vBufferFrameCount,
+	const ::PaStreamCallbackTimeInfo *pTimeInfo,
+	::PaStreamCallbackFlags			  vStatusFlags,
+	void							 *pUserData
+)
+{
+	tSample *pIterO = (tSample *)pBufferO;
+	tSample *pIterI = (tSample *)pBufferI;
+	// write to the buffer
+	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
+	{
+		*(pIterO++) = *(pIterI);
+		*(pIterO++) = *(pIterI);
+		pIterI++;
+	} // write to the buffer
+	return cECodeNone;
+} // fEchoCallback
+int fPrintADevInfo(const ::PaDeviceInfo *pADevInfo)
+{
+	// meta
+	int				 vADevInfoVnum = pADevInfo->structVersion;
+	const char		*vADevName	   = pADevInfo->name;
+	::PaHostApiIndex vADevApiIndex = pADevInfo->hostApi;
+	std::printf("[Vnum]=(%d)=[Vnum]\n", vADevInfoVnum);
+	std::printf("[Name]=(%s)=[Name]\n", vADevName);
+	std::printf("[ApiIndex]=(%d)=[ApiIndex]\n", vADevApiIndex);
+	// chan
+	int vADevIChanCount = pADevInfo->maxInputChannels;
+	int vADevOChanCount = pADevInfo->maxOutputChannels;
+	std::printf("[IChanCount]=(%d)=[IChanCount]\n", vADevIChanCount);
+	std::printf("[OChanCount]=(%d)=[OChanCount]\n", vADevOChanCount);
+	// latency
+	::PaTime vADevILateLower = pADevInfo->defaultLowInputLatency;
+	::PaTime vADevILateUpper = pADevInfo->defaultHighInputLatency;
+	::PaTime vADevOLateLower = pADevInfo->defaultLowOutputLatency;
+	::PaTime vADevOLateUpper = pADevInfo->defaultHighOutputLatency;
+	std::printf("[ILateLower]=(%d)=[ILateLower]\n", vADevILateLower);
+	std::printf("[ILateUpper]=(%d)=[ILateUpper]\n", vADevILateUpper);
+	std::printf("[OLateLower]=(%d)=[OLateLower]\n", vADevOLateLower);
+	std::printf("[OLateUpper]=(%d)=[OLateUpper]\n", vADevOLateUpper);
+	// samples
+	double vADevSampleRate = pADevInfo->defaultSampleRate;
+	std::printf("[SampleRate]=(%d)=[SampleRate]\n", vADevSampleRate);
+	// final
+	return cECodeNone;
+} // fPrintADevInfo
+int fPrintADevInfo(int vADevIndex)
+{
+	std::printf("[%d]=(\n", vADevIndex);
+	const ::PaDeviceInfo *pADevInfo = ::Pa_GetDeviceInfo(vADevIndex);
+	fPrintADevInfo(pADevInfo);
+	std::printf(")=[%d]\n", vADevIndex);
+	return cECodeNone;
+} // fPrintADevInfo
 int main(int argc, const char **argv)
 {
 	// datadef
@@ -84,11 +156,60 @@ int main(int argc, const char **argv)
 		.vPhaseL = cSampleMid,
 		.vPhaseR = cSampleMid,
 	};
+	// // streams
 	::PaStream *pStream = cNull;
+	// // devices
+	int					  vADevCount = 0;
+	int					  vIDevIndex = 0;
+	int					  vODevIndex = 0;
+	const ::PaDeviceInfo *pIDevInfo	 = cNull;
+	const ::PaDeviceInfo *pODevInfo	 = cNull;
+	// // user input
+	char vUserKey = '\0';
+	int	 vUserNum = 0;
 	// actions
 	// // init
 	vECode = ::Pa_Initialize();
 	dProcECode(vECode);
+	// // // devices
+	vADevCount = ::Pa_GetDeviceCount();
+	if(vADevCount < 0) { dProcECode(vADevCount); }
+	std::printf("[ADevCount]=(%d)=[ADevCount]\n", vADevCount);
+	std::printf("[ADevInfoList]=(\n");
+	for(auto vADevIndex = 0; vADevIndex < vADevCount; vADevIndex++)
+	{
+		fPrintADevInfo(vADevIndex);
+	} // process device info
+	std::printf(")=[ADevInfoList]\n");
+	// // // // idev
+	std::printf("\n[IDevIndex]=(");
+	std::cin >> vIDevIndex;
+	std::printf(")=[IDevIndex]\n");
+	if(vIDevIndex >= 0 && vIDevIndex < vADevCount)
+	{
+		fPrintADevInfo(vIDevIndex);
+		pIDevInfo = ::Pa_GetDeviceInfo(vIDevIndex);
+	}
+	else
+	{
+		std::printf("invalid IDevIndex=%d\n", vIDevIndex);
+		return cECodeADevIndex;
+	}
+	// // // // odev
+	std::printf("\n[ODevIndex]=(");
+	std::cin >> vODevIndex;
+	std::printf(")=[ODevIndex]\n");
+	if(vODevIndex >= 0 && vODevIndex < vADevCount)
+	{
+		fPrintADevInfo(vODevIndex);
+		pODevInfo = ::Pa_GetDeviceInfo(vODevIndex);
+	}
+	else
+	{
+		std::printf("invalid ODevIndex=%d\n", vODevIndex);
+		return cECodeADevIndex;
+	}
+	// // // streams
 	vECode = ::Pa_OpenDefaultStream(
 		&pStream,
 		cIChanCount,
@@ -96,14 +217,14 @@ int main(int argc, const char **argv)
 		cSampleEnum,
 		cSampleRate,
 		cBufferFrameCount,
-		&fTestCallback,
+		&fIModifyCallback,
 		&vTData
 	);
 	dProcECode(vECode);
 	// // work
 	vECode = ::Pa_StartStream(pStream);
 	dProcECode(vECode);
-	::Pa_Sleep(3'000);
+	while(vUserKey != 'x') { std::cin >> vUserKey; }
 	vECode = ::Pa_StopStream(pStream);
 	dProcECode(vECode);
 	// // quit
