719d09fe0c91d253a8cc027fb82a838ec7756376
nikozdev <nikozdev@nikozdev.net>
2023-07-15 09:13:23 -0700

    !src!main!test!audio!noise!sin!wave
    [src/main.cxx]
    > refactoring, making pre-declarations and post-definitions
    > trying to implement sine wave
    > implemented random noise callback

--- src/main.cxx
+++ src/main.cxx
@@ -1,10 +1,13 @@
 // headers
 // // standard
+#include <cmath>
+#include <random>
 #include <cstdio>
 #include <iostream>
 #include <limits>
 // // 3rd-party
 #include "../lib/portaudio/include/portaudio.h"
+#include "../lib/lTermArgs/src/lTermArgs.hxx"
 // defines
 #define dProcECode(vECode)                           \
 	({                                               \
@@ -16,9 +19,15 @@
 		}                                            \
 	}) // dProcECode
 // aliases
+// // errors
+using tECode = ::PaError;
+// // samples
 using tSample		= char;
 using tSampleLimits = ::std::numeric_limits<tSample>;
-using tECode		= ::PaError;
+// // random
+using tRandomEng = std::mt19937; // slow but powerful mersenne_twister
+using tRandomDev = std::random_device;
+using tRandomDis = std::uniform_int_distribution<int>;
 // consdef
 constexpr auto cNull = nullptr;
 // // errors
@@ -40,122 +49,69 @@ constexpr tSample cSampleMid = 0;
 constexpr int cSampleNegStep = -1;
 constexpr int cSamplePosStep = +1;
 // typedef
-typedef struct tTestData
+typedef struct tWaveSawData
 {
 	tSample vPhaseL;
 	tSample vPhaseR;
-} tTestData;
+} tWaveSawData;
 // actions
-static int fWaveSawCallback(
+int fRandomCallback(
 	const void						 *pBufferI,
 	void							 *pBufferO,
 	unsigned long					  vBufferFrameCount,
 	const ::PaStreamCallbackTimeInfo *pTimeInfo,
 	::PaStreamCallbackFlags			  vStatusFlags,
 	void							 *pUserData
-)
-{
-	tTestData *pData  = (tTestData *)pUserData;
-	tSample	  *pIterO = (tSample *)pBufferO;
-	tSample	  *pIterI = (tSample *)pBufferI;
-	// write to the buffer
-	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
-	{
-		*(pIterO++) = pData->vPhaseL;
-		*(pIterO++) = pData->vPhaseR;
-		// simple sawtooth phaser that ranges between min and max
-		pData->vPhaseL += cSampleNegStep;
-		if(pData->vPhaseL <= cSampleMin) { pData->vPhaseL -= cSampleMin; }
-		pData->vPhaseR += cSamplePosStep;
-		if(pData->vPhaseR >= cSampleMax) { pData->vPhaseR -= cSampleMax; }
-	} // write to the buffer
-	return cECodeNone;
-} // fWaveSawCallback
-static int fEchoCallback(
+);
+int fWaveSinCallback(
 	const void						 *pBufferI,
 	void							 *pBufferO,
 	unsigned long					  vBufferFrameCount,
 	const ::PaStreamCallbackTimeInfo *pTimeInfo,
 	::PaStreamCallbackFlags			  vStatusFlags,
 	void							 *pUserData
-)
-{
-	tSample *pIterO = (tSample *)pBufferO;
-	tSample *pIterI = (tSample *)pBufferI;
-	// write to the buffer
-	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
-	{
-		*(pIterO++) = *(pIterI);
-		*(pIterO++) = *(pIterI);
-		pIterI++;
-	} // write to the buffer
-	return cECodeNone;
-} // fEchoCallback
-static int fIModifyCallback(
+);
+int fWaveSawCallback(
 	const void						 *pBufferI,
 	void							 *pBufferO,
 	unsigned long					  vBufferFrameCount,
 	const ::PaStreamCallbackTimeInfo *pTimeInfo,
 	::PaStreamCallbackFlags			  vStatusFlags,
 	void							 *pUserData
-)
+);
+int fEchoCallback(
+	const void						 *pBufferI,
+	void							 *pBufferO,
+	unsigned long					  vBufferFrameCount,
+	const ::PaStreamCallbackTimeInfo *pTimeInfo,
+	::PaStreamCallbackFlags			  vStatusFlags,
+	void							 *pUserData
+);
+int fIModifyCallback(
+	const void						 *pBufferI,
+	void							 *pBufferO,
+	unsigned long					  vBufferFrameCount,
+	const ::PaStreamCallbackTimeInfo *pTimeInfo,
+	::PaStreamCallbackFlags			  vStatusFlags,
+	void							 *pUserData
+);
+int fPrintADevInfo(const ::PaDeviceInfo *pADevInfo);
+int fPrintADevInfo(int vADevIndex);
+int main(int argc, char **argv)
 {
-	tSample *pIterO = (tSample *)pBufferO;
-	tSample *pIterI = (tSample *)pBufferI;
-	// write to the buffer
-	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
-	{
-		*(pIterO++) = *(pIterI);
-		*(pIterO++) = *(pIterI);
-		pIterI++;
-	} // write to the buffer
-	return cECodeNone;
-} // fEchoCallback
-int fPrintADevInfo(const ::PaDeviceInfo *pADevInfo)
-{
-	// meta
-	int				 vADevInfoVnum = pADevInfo->structVersion;
-	const char		*vADevName	   = pADevInfo->name;
-	::PaHostApiIndex vADevApiIndex = pADevInfo->hostApi;
-	std::printf("[Vnum]=(%d)=[Vnum]\n", vADevInfoVnum);
-	std::printf("[Name]=(%s)=[Name]\n", vADevName);
-	std::printf("[ApiIndex]=(%d)=[ApiIndex]\n", vADevApiIndex);
-	// chan
-	int vADevIChanCount = pADevInfo->maxInputChannels;
-	int vADevOChanCount = pADevInfo->maxOutputChannels;
-	std::printf("[IChanCount]=(%d)=[IChanCount]\n", vADevIChanCount);
-	std::printf("[OChanCount]=(%d)=[OChanCount]\n", vADevOChanCount);
-	// latency
-	::PaTime vADevILateLower = pADevInfo->defaultLowInputLatency;
-	::PaTime vADevILateUpper = pADevInfo->defaultHighInputLatency;
-	::PaTime vADevOLateLower = pADevInfo->defaultLowOutputLatency;
-	::PaTime vADevOLateUpper = pADevInfo->defaultHighOutputLatency;
-	std::printf("[ILateLower]=(%d)=[ILateLower]\n", vADevILateLower);
-	std::printf("[ILateUpper]=(%d)=[ILateUpper]\n", vADevILateUpper);
-	std::printf("[OLateLower]=(%d)=[OLateLower]\n", vADevOLateLower);
-	std::printf("[OLateUpper]=(%d)=[OLateUpper]\n", vADevOLateUpper);
-	// samples
-	double vADevSampleRate = pADevInfo->defaultSampleRate;
-	std::printf("[SampleRate]=(%d)=[SampleRate]\n", vADevSampleRate);
-	// final
-	return cECodeNone;
-} // fPrintADevInfo
-int fPrintADevInfo(int vADevIndex)
-{
-	std::printf("[%d]=(\n", vADevIndex);
-	const ::PaDeviceInfo *pADevInfo = ::Pa_GetDeviceInfo(vADevIndex);
-	fPrintADevInfo(pADevInfo);
-	std::printf(")=[%d]\n", vADevIndex);
-	return cECodeNone;
-} // fPrintADevInfo
-int main(int argc, const char **argv)
-{
+	::args::ArgParser vTermArgs{
+		R"(aSoundAgent - a command line digital audio signal processor)",
+		"1.1.0"
+	}; // vTermArgs
+	vTermArgs.parse(argc, argv);
 	// datadef
-	tECode	  vECode = cECodeNone;
-	tTestData vTData = {
+	tECode		 vECode		  = cECodeNone;
+	tWaveSawData vWaveSawData = {
 		.vPhaseL = cSampleMid,
 		.vPhaseR = cSampleMid,
 	};
+	tRandomDev vRandomDev;
+	tRandomEng vRandomEng{ vRandomDev() };
 	// // streams
 	::PaStream *pStream = cNull;
 	// // devices
@@ -217,14 +173,29 @@ int main(int argc, const char **argv)
 		cSampleEnum,
 		cSampleRate,
 		cBufferFrameCount,
+#if 0
+#elif 0
+		&fWaveSinCallback,
+		cNull
+#elif 0
+		&fWaveSawCallback,
+		&vWaveSawData
+#elif 0
+		&fEchoCallback,
+		cNull
+#elif 0
 		&fIModifyCallback,
-		&vTData
+		cNull
+#else
+		&fRandomCallback,
+		&vRandomEng
+#endif
 	);
 	dProcECode(vECode);
 	// // work
 	vECode = ::Pa_StartStream(pStream);
 	dProcECode(vECode);
-	while(vUserKey != 'x') { std::cin >> vUserKey; }
+	while(vUserKey != 'q') { std::cin >> vUserKey; }
 	vECode = ::Pa_StopStream(pStream);
 	dProcECode(vECode);
 	// // quit
@@ -234,4 +205,148 @@ int main(int argc, const char **argv)
 	dProcECode(vECode);
 	// final
 	return vECode;
-}
+} // main
+int fPrintADevInfo(const ::PaDeviceInfo *pADevInfo)
+{
+	// meta
+	int				 vADevInfoVnum = pADevInfo->structVersion;
+	const char		*vADevName	   = pADevInfo->name;
+	::PaHostApiIndex vADevApiIndex = pADevInfo->hostApi;
+	std::printf("[Vnum]=(%d)=[Vnum]\n", vADevInfoVnum);
+	std::printf("[Name]=(%s)=[Name]\n", vADevName);
+	std::printf("[ApiIndex]=(%d)=[ApiIndex]\n", vADevApiIndex);
+	// chan
+	int vADevIChanCount = pADevInfo->maxInputChannels;
+	int vADevOChanCount = pADevInfo->maxOutputChannels;
+	std::printf("[IChanCount]=(%d)=[IChanCount]\n", vADevIChanCount);
+	std::printf("[OChanCount]=(%d)=[OChanCount]\n", vADevOChanCount);
+	// latency
+	::PaTime vADevILateLower = pADevInfo->defaultLowInputLatency;
+	::PaTime vADevILateUpper = pADevInfo->defaultHighInputLatency;
+	::PaTime vADevOLateLower = pADevInfo->defaultLowOutputLatency;
+	::PaTime vADevOLateUpper = pADevInfo->defaultHighOutputLatency;
+	std::printf("[ILateLower]=(%d)=[ILateLower]\n", vADevILateLower);
+	std::printf("[ILateUpper]=(%d)=[ILateUpper]\n", vADevILateUpper);
+	std::printf("[OLateLower]=(%d)=[OLateLower]\n", vADevOLateLower);
+	std::printf("[OLateUpper]=(%d)=[OLateUpper]\n", vADevOLateUpper);
+	// samples
+	double vADevSampleRate = pADevInfo->defaultSampleRate;
+	std::printf("[SampleRate]=(%d)=[SampleRate]\n", vADevSampleRate);
+	// final
+	return cECodeNone;
+} // fPrintADevInfo
+int fPrintADevInfo(int vADevIndex)
+{
+	std::printf("[%d]=(\n", vADevIndex);
+	const ::PaDeviceInfo *pADevInfo = ::Pa_GetDeviceInfo(vADevIndex);
+	fPrintADevInfo(pADevInfo);
+	std::printf(")=[%d]\n", vADevIndex);
+	return cECodeNone;
+} // fPrintADevInfo
+int fRandomCallback(
+	const void						 *pBufferI,
+	void							 *pBufferO,
+	unsigned long					  vBufferFrameCount,
+	const ::PaStreamCallbackTimeInfo *pTimeInfo,
+	::PaStreamCallbackFlags			  vStatusFlags,
+	void							 *pUserData
+)
+{
+	tSample	   *pIterO	   = (tSample *)pBufferO;
+	tSample	   *pIterI	   = (tSample *)pBufferI;
+	tRandomEng *pRandomEng = (tRandomEng *)pUserData;
+	tRandomDis	vRandomDis{ cSampleMin, cSampleMax };
+	// write to the buffer
+	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
+	{
+		*(pIterO++) = vRandomDis(*pRandomEng);
+		*(pIterO++) = vRandomDis(*pRandomEng);
+	} // write to the buffer
+	return cECodeNone;
+} // fRandomCallback
+int fWaveSinCallback(
+	const void						 *pBufferI,
+	void							 *pBufferO,
+	unsigned long					  vBufferFrameCount,
+	const ::PaStreamCallbackTimeInfo *pTimeInfo,
+	::PaStreamCallbackFlags			  vStatusFlags,
+	void							 *pUserData
+)
+{
+	tSample *pIterO = (tSample *)pBufferO;
+	tSample *pIterI = (tSample *)pBufferI;
+	// write to the buffer
+	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
+	{
+		auto vTime	= (float)pTimeInfo->currentTime;
+		auto vRad	= vTime * M_PI / 180.0;
+		*(pIterO++) = (tSample)(std::sin(vRad) * cSampleMax);
+		*(pIterO++) = (tSample)(std::sin(vRad) * cSampleMax);
+	} // write to the buffer
+	return cECodeNone;
+} // fWaveSinCallback
+int fWaveSawCallback(
+	const void						 *pBufferI,
+	void							 *pBufferO,
+	unsigned long					  vBufferFrameCount,
+	const ::PaStreamCallbackTimeInfo *pTimeInfo,
+	::PaStreamCallbackFlags			  vStatusFlags,
+	void							 *pUserData
+)
+{
+	tWaveSawData *pData	 = (tWaveSawData *)pUserData;
+	tSample		 *pIterO = (tSample *)pBufferO;
+	tSample		 *pIterI = (tSample *)pBufferI;
+	// write to the buffer
+	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
+	{
+		*(pIterO++) = pData->vPhaseL;
+		*(pIterO++) = pData->vPhaseR;
+		// simple sawtooth phaser that ranges between min and max
+		pData->vPhaseL += cSampleNegStep;
+		if(pData->vPhaseL <= cSampleMin) { pData->vPhaseL -= cSampleMin; }
+		pData->vPhaseR += cSamplePosStep;
+		if(pData->vPhaseR >= cSampleMax) { pData->vPhaseR -= cSampleMax; }
+	} // write to the buffer
+	return cECodeNone;
+} // fWaveSawCallback
+int fEchoCallback(
+	const void						 *pBufferI,
+	void							 *pBufferO,
+	unsigned long					  vBufferFrameCount,
+	const ::PaStreamCallbackTimeInfo *pTimeInfo,
+	::PaStreamCallbackFlags			  vStatusFlags,
+	void							 *pUserData
+)
+{
+	tSample *pIterO = (tSample *)pBufferO;
+	tSample *pIterI = (tSample *)pBufferI;
+	// write to the buffer
+	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
+	{
+		*(pIterO++) = *(pIterI);
+		*(pIterO++) = *(pIterI);
+		pIterI++;
+	} // write to the buffer
+	return cECodeNone;
+} // fEchoCallback
+int fIModifyCallback(
+	const void						 *pBufferI,
+	void							 *pBufferO,
+	unsigned long					  vBufferFrameCount,
+	const ::PaStreamCallbackTimeInfo *pTimeInfo,
+	::PaStreamCallbackFlags			  vStatusFlags,
+	void							 *pUserData
+)
+{
+	tSample *pIterO = (tSample *)pBufferO;
+	tSample *pIterI = (tSample *)pBufferI;
+	// write to the buffer
+	for(unsigned vIter = 0; vIter < vBufferFrameCount; vIter++)
+	{
+		*(pIterO++) = *(pIterI);
+		*(pIterO++) = *(pIterI);
+		pIterI++;
+	} // write to the buffer
+	return cECodeNone;
+} // fModifyCallback
